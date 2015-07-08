990c0139a850a08793ac5ce9078bb904c02fb2d7
Connor Workman <cworkman@cs.uoregon.edu>
2015-07-07 17:11:04 -0700

    Changed puppet.conf and site.pp after resetting hostname configurations to default.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,9 @@
 #connor-trusty
-node connor-trusty {
+node ip-10-0-4-226 {
 }
 #joeyubuntu
-node joeyubuntu {
+node ip-10-0-4-27 {
 }
 #ubuntu-trusty-ec2-reedrs
-node ubuntu-trusty-ec2-reedrs {
+node ip-10-0-4-230 {
 }
