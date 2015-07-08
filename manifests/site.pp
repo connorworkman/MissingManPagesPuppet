9eb5968cdcdebe606ae9504e4213db2398e8242a
Connor Workman <cworkman@cs.uoregon.edu>
2015-07-07 18:01:30 -0700

    Removed -q flag to test cron job

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,7 +7,7 @@ node ip-10-0-4-27 {
 #puppetmaster (reed's instance)
 node ip-10-0-4-230 {
 	cron { "puppet update":
-		command	=> "cd /etc/puppet && git pull -q origin master",
+		command	=> "cd /etc/puppet && git pull origin master",
 		user	=> root,
 		minute	=> "*/5",
 	}
