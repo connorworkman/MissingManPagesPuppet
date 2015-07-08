94f3970de2665d88cbca18ac3cea16b1e45a8370
Reed Reese-Steiner <arleycat@gmail.com>
2015-07-08 15:37:51 -0700

    Copied init.pp from Steve's projector and attempting to use it

---
+++ modules/sshd/manifests/init.pp
@@ -0,0 +1,21 @@
+class sshd {
+	package {
+		"openssh-server": ensure ==> installed;
+	}
+
+	file { "/etc/ssh/sshd_config":
+		mode	=> 444,
+		owner	=> "root",
+		group	=> "root",
+		source	=> "puppet:///modules/sshd/sshd_config",
+		require => Package["openssh-server"],
+	}
+
+	service { "ssh":
+		enable	=> true,
+		ensure	=> running,
+		require	=>	[ Package[""openssh-server"],
+				  File["/etc/ssh/sshd_config], ],
+	}
+}
+
