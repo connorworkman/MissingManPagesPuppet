93975f9452780ec595a4bb82357f4e213199e454
Connor Workman <cworkman@cs.uoregon.edu>
2015-07-14 19:41:26 -0700

    Added the httpd (apache2) module for my instance. Default configuration

---
+++ modules/httpd-connor/manifests/init.pp
@@ -0,0 +1,20 @@
+class httpd {
+	package {
+		"apache2": ensure => installed;
+	}
+
+	file { "/etc/apache2/apache2.conf":
+		mode	=> 644,
+		owner	=> "root",
+		group	=> "root",
+		source	=> "puppet:///modules/httpd/apache2.conf",
+		require => Package["apache2"],
+	}
+
+	service { "apache2":
+		enable	=> true,
+		ensure	=> running,
+		require	=>	[ Package["apache2"],
+				  File["/etc/apache2/apache2.conf"], ],
+	}
+}
