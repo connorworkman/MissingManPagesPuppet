7cc0f1273ea789077433bad5859015323b4a93bb
Connor Workman <cworkman@cs.uoregon.edu>
2015-07-26 16:38:34 +0000

    Narrowing in on working PHP host printing

--- modules/lighttpd/files/index.php
+++ modules/lighttpd/files/index.php
@@ -15,9 +15,9 @@ The Missing Man Pages - Load Balanced HTTP Server</font></p>
  <p>This page may be served by either Joey's or Connor's machine.</p>
  <p><img src="./mmp.gif" alt="MMP"></p>
 
-<?php $hostname = gethostbyaddr($_SERVER['X-Proxy']);
+<?php $hostname = getResponseHeader(['X-Proxy']);
 echo "<br>";
-if ($hostname == "ip-10-0-4-29.us-west-2.compute.internal") {
+if ($hostname == "ip-10-0-4-226.us-west-2.compute.internal") {
         echo $hostname;
         echo "<br>";
         echo "This page is currently being served from Connor's instance.";
