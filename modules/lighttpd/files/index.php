2cfb6411f339a1ac3d21b6995114b5487ae21cf3
Reed Reese-Steiner <reedr@uoregon.edu>
2015-07-26 17:38:08 -0700

    Trying out some different code to decide which instance is serving the page

--- modules/lighttpd/files/index.php
+++ modules/lighttpd/files/index.php
@@ -14,11 +14,24 @@ The Missing Man Pages - Load Balanced HTTP Server</font></p>
 <font face="arial">
  <p>This page may be served by either Joey's or Connor's machine.</p>
  <p><img src="./mmp.gif" alt="MMP"></p>
-<?php
-$hostname = HttpRequest::getResponseHeader(["X-Proxy"]);
-echo $hostname;
-echo "<br>";
-?>
+
+ <?php
+  $hostname=gethostname();
+
+  switch ($hostname) {
+   case "ip-10-0-4-226":
+    $instance_name="Connor";
+    break;
+   case "ip-10-0-4-27";
+    $instance_name="Joey";
+    break;
+   default:
+    $instance_name="someone else (this shouldn't happen)";
+    break;
+   }
+
+   echo "Owner of the instance serving this page: $instance_name"
+ ?>
 </font>
 </BODY>
 </html>
