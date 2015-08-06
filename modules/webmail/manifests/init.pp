e87d0a035349f582f5999949880cce0e0aa01e02
Connor Workman <cworkman@cs.uoregon.edu>
2015-08-06 14:36:36 -0700

    Needed to add temp and logs folders with web server group access privileges.  Also removed purge option from /var/www/roundcube*

--- modules/webmail/manifests/init.pp
+++ modules/webmail/manifests/init.pp
@@ -2,11 +2,25 @@ class webmail {
     	file { "/var/www/roundcubemail-1.1.2/":
         	ensure	=> directory,
         	recurse => true,
-        	purge   => true,
+        	purge   => false,
         	force   => true,
         	owner   => "root",
         	group   => "root",
         	mode    => 0644,
         	source  => "puppet:///modules/webmail/roundcubemail-1.1.2/",
     	}
+        #add folders with web server access privileges
+        file {"/var/www/roundcubemail-1.1.2/temp/":
+            ensure  => directory,
+            owner   => "www-data",
+            group   => "www-data",
+            mode    => 0750,
+        }
+
+        file {"/var/www/roundcubemail-1.1.2/logs/":
+            ensure  => directory,
+            owner   => "www-data",
+            group   => "www-data",
+            mode    => 0750,
+        }
 }
