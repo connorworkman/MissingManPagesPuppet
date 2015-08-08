54784cc86fde8876c0fc7aae920ecc81ad26c2e0
Connor Workman <cworkman@cs.uoregon.edu>
2015-08-08 14:39:00 -0700

    Added some service requirements and dovecot configuration files.

--- modules/webmail/manifests/init.pp
+++ modules/webmail/manifests/init.pp
@@ -9,6 +9,8 @@ class webmail {
         	mode    => 0644,
         	source  => "puppet:///modules/webmail/roundcubemail-1.1.2/",
     	}
+
+
         #add folders with web server access privileges
         file {"/var/www/roundcubemail-1.1.2/temp/":
             ensure  => directory,
@@ -23,15 +25,41 @@ class webmail {
             group   => "www-data",
             mode    => 0750,
         }
-	#require package dovecot
-	package { "dovecot-imapd":
-		ensure	=> installed,
-	}
-	package { "dovecot-mysql":
-		ensure	=> installed,
-	}
-	#require configuration-files dovecot (includes /etc/dovecot/....)
-	#require package php5-fpm
-	#require configuration-files php5-fpm (includes /etc/php5-fpm/....)
+	    
+        #require packages for dovecot
+	    package { "dovecot-imapd":
+	    	ensure	=> installed,
+	    }
+	    package { "dovecot-mysql":
+	    	ensure	=> installed,
+	    }
+
+	    #require configuration-files for dovecot (includes /etc/dovecot/....)
+        file {"/etc/dovecot/dovecot.conf":
+            mode    => 0644,
+            owner   => "root",
+            group   => "root",
+            source  => "puppet:///modules/webmail/dovecot/dovecot.conf",
+            notify  => Service["dovecot"],
+        }
+
+        file { "/etc/dovecot/conf.d/":
+            ensure  => directory,
+            recurse => true,
+            purge   => false,
+            force   => true,
+            owner   => "root",
+            group   => "root",
+            mode    => 0644,
+            source  => "puppet:///modules/webmail/dovecot/conf.d",
+        }
+        service {"dovecot":
+            enable  => true,
+            ensure  => running,
+            require => [ Package["dovecot-imapd"], Package["dovecot-mysql"] ],
+            subscribe => File["/etc/dovecot/dovecot.conf"],
+        }
+	    #require package php5-fpm
+	    #require configuration-files php5-fpm (includes /etc/php5-fpm/....)
 	
 }
