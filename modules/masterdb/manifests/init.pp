22bdfe6e7a117a5a9cc65af413b0dc16ca4f4c0c
Connor Workman <cworkman@cs.uoregon.edu>
2015-08-08 18:50:36 -0700

    Added package dependencies

--- modules/masterdb/manifests/init.pp
+++ modules/masterdb/manifests/init.pp
@@ -2,6 +2,12 @@ class masterdb {
     package {"mysql-server-5.6":
         ensure  => installed,
     }
+    package {"mysql-client-5.6":
+        ensure  => installed,
+    }
+    package {"mysql-server-core-5.6":
+        ensure  => installed,
+    }
     service {"mysql":
         enable  => true,
         ensure  => running,
