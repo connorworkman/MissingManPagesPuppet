846a90b95882bc1143c8422a65f1c4c3b26731ec
Connor Workman <cworkman@cs.uoregon.edu>
2015-08-09 01:07:10 -0700

    Restoring working version without memcache

--- modules/webmail/files/ip-10-0-4-226/config.inc.php
+++ modules/webmail/files/ip-10-0-4-226/config.inc.php
@@ -56,8 +56,8 @@ $config['des_key'] = '928xd121xd934d49b11c3fxd';
 
 
 
-$config['session_storage'] = 'memcache';
-$config['memcache_hosts'] = array( 'ip-10-0-4-230:11211');
+//$config['session_storage'] = 'memcache';
+//$config['memcache_hosts'] = array( 'ip-10-0-4-230:11211');
 
 
 // Name your service. This is displayed on the login screen and in the window title
--- modules/webmail/files/ip-10-0-4-27/config.inc.php
+++ modules/webmail/files/ip-10-0-4-27/config.inc.php
@@ -48,8 +48,8 @@ $config['auto_create_user'] = true;
 // please provide a string of exactly 24 chars.
 $config['des_key'] = '928xd121xd934d49b11c3fxd';
 
-$config['session_storage'] = 'memcache';
-$config['memcache_hosts'] = array( 'ip-10-0-4-230:11211' );
+//$config['session_storage'] = 'memcache';
+//$config['memcache_hosts'] = array( 'ip-10-0-4-230:11211' );
 
 
 
