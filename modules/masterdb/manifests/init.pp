9b996044ce61d4cf6c7b540e054e3bc48ee4b931
Connor Workman <cworkman@cs.uoregon.edu>
2015-08-08 19:06:23 -0700

    Removed redundant package requirements (they're already dependencies) and corrected package name

--- modules/masterdb/manifests/init.pp
+++ modules/masterdb/manifests/init.pp
@@ -1,16 +1,10 @@
 class masterdb {
-    package {"mysql-server-5.6":
-        ensure  => installed,
-    }
-    package {"mysql-client-5.6":
-        ensure  => installed,
-    }
-    package {"mysql-server-core-5.6":
+    package {"mysql-server":
         ensure  => installed,
     }
     service {"mysql":
         enable  => true,
         ensure  => running,
-        require => Package["mysql-server-5.6"],
+        require => Package["mysql-server"],
     }
 }
