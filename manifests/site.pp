3a2fdc87f3c3279e1fc27704f866513364004f83
Connor Workman <connorworkman@users.noreply.github.com>
2016-10-18 00:10:06 -0500

    Indentation fix

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,10 +2,10 @@ import 'classes/*.pp'
 
 #connor's instance
 node ip-10-0-4-226 {
-	include sshd
-#	include lighttpd
-	include sendmail
-#	include webmail
+    include sshd
+#   include lighttpd
+    include sendmail
+#   include webmail
     include phpmod
     include memcachemod
     include dovecotmod
@@ -13,10 +13,10 @@ node ip-10-0-4-226 {
 
 #joey's instance
 node ip-10-0-4-27 {
-	include sshd
-#	include lighttpd
-	include apache2
-#	include sendmail
+    include sshd
+#   include lighttpd
+    include apache2
+#   include sendmail
     include webmail
     include phpmod
     include memcachemod
@@ -24,16 +24,16 @@ node ip-10-0-4-27 {
 
 #puppetmaster (reed's instance)
 node ip-10-0-4-230 {
-	cron { "puppet update":
-		command	=> "cd /etc/puppet && git pull origin master && echo 'Update occurred.'",
-		user	=> root,
-		minute	=> "*/5",
-	}
+    cron { "puppet update":
+        command	=> "cd /etc/puppet && git pull origin master && echo 'Update occurred.'",
+        user	=> root,
+        minute	=> "*/5",
+    }
     include masterdb
-	include ldap
+    include ldap
     include phpmod
     include memcachemod
     include sshd
-	include sendmail
-	include lighttpd-reed
+    include sendmail
+    include lighttpd-reed
 }
